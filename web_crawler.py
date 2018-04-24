77d4f02e4c4e44a09ce6d23f1611fb2e571990d4
l290w868 <l290w868@ku.edu>
2018-04-24 07:39:17 -0500

    added try catch block to catch all the unresponding urls

--- web_crawler.py
+++ web_crawler.py
@@ -6,6 +6,7 @@
 import requests
 from bs4 import BeautifulSoup
 from Queue import Queue
+from multiprocessing.pool import ThreadPool
 from pre_processing import progressbar
 
 
@@ -16,32 +17,39 @@ from pre_processing import progressbar
 """
 def url_scraping(url, frontier, visited_repo, limit):
 	# request will time out after 1 second if its not responding
-	page = requests.get(url, timeout=1)
-	soup = BeautifulSoup(page.text, "lxml")
+	try:
+		page = requests.get(url, timeout=1)
+		soup = BeautifulSoup(page.text, "lxml")
 
-	# recursively enqueue the link
-	for link in soup.find_all('a'):
-		url = link.get('href')
-		# make sure the link is valid
-		if url.startswith("http") if url else False and not frontier.full():
-			frontier.put(url)
+		# recursively enqueue the link
+		for link in soup.find_all('a'):
+			url = link.get('href')
+			# make sure the link is valid
+			if url.startswith("http") if url else False and not frontier.full():
+				frontier.put(url)
 
-	# keeps crawling when the frontier is not empty
-	while not frontier.empty():
-		# dequeue the frontier
-		next_url = frontier.get()
-		if len(visited_repo) < limit:
-			# make sure the item is not duplicated
-			if next_url not in visited_repo:
-				#  mark the dequeued item as visited
-				visited_repo.append(next_url)
-		else:
-			# return when the repo reaches the limit
-			return frontier, visited_repo
-		# continue scraping urls if the frontier hasn't reach the limit
-		if frontier.qsize() <= limit:
-			# recursively call the scraping method
-			url_scraping(next_url, frontier, visited_repo, limit)
+		# keeps crawling when the frontier is not empty
+		while not frontier.empty():
+			# dequeue the frontier
+			next_url = frontier.get()
+			if len(visited_repo) < limit:
+				# make sure the item is not duplicated
+				if next_url not in visited_repo:
+					#  mark the dequeued item as visited
+					visited_repo.append(next_url)
+					# indicate the crawling progress
+					# progressbar(len(visited_repo), limit, prefix = 'Progress:', length = 50)
+			else:
+				# return when the repo reaches the limit
+				return frontier, visited_repo
+			# continue scraping urls if the frontier hasn't reach the limit
+			if frontier.qsize() <= limit:
+				# recursively call the scraping method
+				url_scraping(next_url, frontier, visited_repo, limit)
+	# catch the exception if any url is not responding
+	except Exception as e:
+		print e.message
+    	return
 
 
 """
@@ -50,18 +58,37 @@ def url_scraping(url, frontier, visited_repo, limit):
 	return: NONE
 """
 def spider(root_url, limit):
-	# set the frontier with a limit
+	# initialize the frontier and url repo
 	frontier = Queue(maxsize=0)
 	visited_repo = []
 
 	frontier, visited_repo = url_scraping(root_url, frontier, visited_repo, limit)
-	print "frontier:", frontier.qsize()
-	print "visitted", visited_repo
+	print "\nfrontier:", frontier.qsize()
+	print "\nvisitted", visited_repo
+	print "\nvisitted size: ", len(visited_repo)
 
 
 def main():
-	root_url = 'http://www.leiwangcoding.com'
-	spider(root_url, 100)
+	root_url = 'http://www.ku.edu'
+	spider(root_url, 1000)
+
+	# creating threads
+	# t1 = Thread(target=spider, args=(root_url, 10))
+	# t2 = Thread(target=spider, args=(root_url, 10))
+	# t3 = Thread(target=spider, args=(root_url, 10))
+	# t4 = Thread(target=spider, args=(root_url, 10))
+
+	# # starting threads
+	# t1.start()
+	# t2.start()
+	# t3.start()
+	# t4.start()
+
+	# # wait until all threads finish
+	# t1.join()
+	# t2.join()
+	# t3.join()
+	# t4.join()
 
 
 if __name__ == '__main__':
