9ab71006789dcb2d022e2af383c2bec2b5e53387
l290w868 <l290w868@ku.edu>
2018-05-02 02:24:20 -0500

    now everything is up to date and ready for UI intergration

--- engine_core/search.py
+++ engine_core/search.py
@@ -15,14 +15,14 @@ import pickle
 """
 def read_data():
 	dictionary, postings_list, idf = [], [], []
-
+	# reading dictionary to a list
    	dictionary = json.load(open('dictionary.txt'))
-
+   	# reading postings to a list
    	with open('postings.pkl', 'rb') as input:
    		postings = pickle.load(input)
    		for posting in postings:
   			postings_list.append(posting)
-
+  	# reading idf to a list
 	with open("idf.txt") as filehandle:
 		for line in filehandle:
 			idf.append(float(line))
@@ -34,24 +34,21 @@ def search():
 	dictionary, postings, idf = [], [], []
 
 	dictionary, postings, idf = read_data()
-	print dictionary, postings, idf
 
 	print_table(dictionary, postings)
 
+	while True:
+		query = raw_input(bcolors.BOLD + bcolors.OKGREEN + '\n\nEnter your query to search:(type q to quit search) \n'.upper() + bcolors.ENDC).lower()
+		if query == 'q':
+			print "goodbye!"
+			exit()
+		else:
+			print bcolors.BOLD + bcolors.OKGREEN + "\n\ncalculating document rankings".upper() + bcolors.ENDC
+			processed_query = stopword_removal(query)
+			processed_query = stemmer(processed_query)
 
-	# query = raw_input(bcolors.BOLD + bcolors.OKGREEN + '\n\nEnter your query to search:(type q to quit search) \n'.upper() + bcolors.ENDC).lower()
-
-	# while True:
-	# 	if query == 'q':
-	# 		print "goodbye!"
-	# 		exit()
-	# 	else:
-	# 		print bcolors.BOLD + bcolors.OKGREEN + "\n\ncalculating document rankings".upper() + bcolors.ENDC
-	# 		processed_query = stopword_removal(query)
-	# 		processed_query = stemmer(processed_query)
-
-	# 	# similarity_score = query_processing(processed_query, dictionary, weight_matrix, filenames)
-	# 	similarity_score = query_search(processed_query, dictionary, postings, idf)
+		# similarity_score = query_processing(processed_query, dictionary, weight_matrix, filenames)
+		similarity_score = query_search(processed_query, dictionary, postings, idf)
 
 
 
