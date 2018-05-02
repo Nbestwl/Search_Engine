5336bb0662f14553329109bccc54b5b2d08a87ac
l290w868 <l290w868@ku.edu>
2018-05-01 20:36:59 -0500

    added simple UI and style for the searching field

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-	return 'ok'
+def my_form():
+    return render_template('search_engine.html')
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    processed_text = text.upper()
+    return processed_text
 
 if __name__ == '__main__':
 	app.run()
